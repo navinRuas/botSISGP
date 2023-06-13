cb86908916f9ea6d4824f60447147d4d06224411
navinRuas <navinchandry.ruas@inep.gov.br>
2023-06-13 10:01:22 -0300

    sisgpSQL atualização

--- sisgpSQL.py
+++ sisgpSQL.py
@@ -1,5 +1,6 @@
 import json
 from Conexao import pontalina
+from extraUtils import stripFunc
 import decimal
 import datetime
 
@@ -12,6 +13,13 @@ def handler(obj):
 
 def sisgpSQL():
     dados = pontalina('SELECT [NomeServidor],[SiglaUnidade],[NomeUnidade],[tfnDescricao],[SituacaoPactoTrabalho],[TipoPGD],[titulo],[pactoTrabalhoId],[planoTrabalhoId],[DtInicioPactoTrab],[DtFimPactoTrab],[tempoComparecimento], [cargaHorariaDiaria], [percentualExecucao], [relacaoPrevistoRealizado], [avaliacaoId], [tempoTotalDisponivel], [quantidade], [tempoPrevistoPorItem], [tempoPrevistoTotal], [DtInicioPactoTrabAtividade], [DtFimPactoTrabAtividade], [tempoRealizado], [SituaçãoAtividade], [descricao], [tempoHomologado], [nota], [justificativa], [consideracoesConclusao] FROM [ProgramaGestao].[VW_PlanoTrabalhoAUDIN] WHERE SituacaoPactoTrabalho = \'Em execução\' OR SituacaoPactoTrabalho =\'Autorizado\'')
+    for item in dados:
+        demanda_value = stripFunc(item['descricao'], 'demanda')
+        print(demanda_value)
+        item['Demanda'] = demanda_value
+
+        item['Atividade'] = stripFunc(item['descricao'], 'atividade')
+        item['Produto'] = stripFunc(item['descricao'], 'produto')
 
     # Salva linhas em um arquivo data.json no OneDrive, sobrepondo o arquivo existente
     with open('C:\\Users\\navinchandry.ruas\\OneDrive - INEP\\sisGP\\data.json', 'w') as outfile:
